99df02e891cca62f49441ab33f9b64fd58fc055a
Phạm Trần Đức Trọng <trong.phamtranduc@gmail.com>
2024-11-13 14:30:13 +0700

    update

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "pong 123",
 		})
 	})
 	r.Run(":" + PORT)
